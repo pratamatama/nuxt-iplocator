2d4c4410c1f23876a70d9db8beb77223c56a57b4
Donny Pratama <donnypratama1024@gmail.com>
2022-03-12 22:39:50 +0700

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  mounted() {
+    this.fetchIpaddress()
+  },
+  methods: {
+    async fetchIpaddress() {
+      try {
+        const data = await this.$axios.$get(
+          'https://api.ipregistry.co?key=han4um8qabhqcg00'
+        )
+        // eslint-disable-next-line
+        console.log(data)
+
+        const response = await this.$axios.$post('/api/ip-addresses', {
+          data: {
+            ipaddress: data,
+          },
+        })
+        // eslint-disable-next-line
+        console.log(response)
+      } catch (err) {
+        // eslint-disable-next-line
+        console.error(err)
+      }
+    },
+  },
+}
+</script>
